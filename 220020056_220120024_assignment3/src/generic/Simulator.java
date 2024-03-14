b5c2719bc762dc71fbaabef43dd66be3e1d9c684
subhash2405 <subhashchandrap456@gmail.com>
2024-03-14 23:41:54 +0530

    x

---
+++ 220020056_220120024_assignment3/src/generic/Simulator.java
@@ -0,0 +1,86 @@
+package generic;
+
+import processor.Clock;
+import processor.Processor;
+import java.util.*;
+import java.io.*;
+
+public class Simulator {
+		
+	static Processor processor;
+	static boolean simulationComplete;
+	
+	public static void setupSimulation(String assemblyProgramFile, Processor p)
+	{
+		Simulator.processor = p;
+		loadProgram(assemblyProgramFile);
+		
+		simulationComplete = false;
+	}
+	
+	static void loadProgram(String assemblyProgramFile)
+	{
+		/*
+		 * TODO
+		 * 1. load the program into memory according to the program layout described
+		 *    in the ISA specification
+		 * 2. set PC to the address of the first instruction in the main
+		 * 3. set the following registers:
+		 *     x0 = 0
+		 *     x1 = 65535
+		 *     x2 = 65535
+		 */
+		try{
+			FileInputStream file = new FileInputStream(assemblyProgramFile);
+			DataInputStream line = new DataInputStream(file);
+			int next = 0;
+			if(line.available()>0){
+				next=line.readInt();
+				processor.getRegisterFile().setProgramCounter(next);
+			}
+			for(int i=0;line.available()>0;i++){
+				next=line.readInt();
+				processor.getMainMemory().setWord(i,next);
+			}
+
+			processor.getRegisterFile().setValue(0,0);
+			processor.getRegisterFile().setValue(1,65535);
+			processor.getRegisterFile().setValue(2,65535);
+			line.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static void simulate()
+	{
+		int numinst=0;
+		int numcycles=0;
+		while(simulationComplete == false)
+		{
+			processor.getRWUnit().performRW();
+			processor.getMAUnit().performMA();
+			processor.getEXUnit().performEX();
+			processor.getOFUnit().performOF();
+			processor.getIFUnit().performIF();
+			Clock.incrementClock();
+			numinst+=5;    //check if its correct
+			numcycles+=1;
+		}
+		
+		// TODO
+		// set statistics
+		Statistics.setNumberOfInstructions(numinst);
+		Statistics.setNumberOfCycles(numcycles);
+		// Statistics.setCPI(numinst, numcycles);
+
+	}
+
+	
+	public static void setSimulationComplete(boolean value)
+	{
+		simulationComplete = value;
+	}
+}
---
+++ 220020056_220120024_assignment3/src/processor/pipeline/OperandFetch.java
@@ -0,0 +1,213 @@
+package processor.pipeline;
+
+import generic.Instruction;
+import generic.Operand;
+import generic.Instruction.OperationType;
+import generic.Operand.OperandType;
+import processor.Processor;
+
+public class OperandFetch {
+	Processor containingProcessor;
+	IF_OF_LatchType IF_OF_Latch;
+	OF_EX_LatchType OF_EX_Latch;
+	
+	public OperandFetch(Processor containingProcessor, IF_OF_LatchType iF_OF_Latch, OF_EX_LatchType oF_EX_Latch)
+	{
+		this.containingProcessor = containingProcessor;
+		this.IF_OF_Latch = iF_OF_Latch;
+		this.OF_EX_Latch = oF_EX_Latch;
+	}
+	public String twoscomplement(String bin) {
+		String ones = "";
+		for (int i = 0; i < bin.length(); i++) {
+			if (bin.charAt(i) == '1') {
+				ones += '0';
+			} else {
+				ones += '1';
+			}
+		}
+		
+		String twos = "";
+		boolean carry = true; 
+		for (int i = bin.length() - 1; i >= 0; i--) {
+			if (ones.charAt(i) == '1' && carry) {
+				twos = '0' + twos; // Add 0 when there's a carry
+			} else if (ones.charAt(i) == '0' && carry) {
+				twos = '1' + twos; // Add 1 when there's no carry
+				carry = false; // Reset carry flag
+			} else {
+				twos = ones.charAt(i) + twos; // Add the remaining bits
+			}
+		}
+		return twos;
+	}
+	public void performOF() 
+	{
+		if(IF_OF_Latch.isOF_enable())
+		{
+			//TOD
+			Instruction newinst = new Instruction();
+			String inst = Integer.toBinaryString(IF_OF_Latch.getInstruction());
+			int csize = inst.length();
+			csize = 32-csize;
+			for(int i=1;i<=csize;i++)
+				inst = '0' + inst;
+			String opCode = inst.substring(0,5);
+			OperationType[] operations = OperationType.values();
+			OperationType operation = operations[Integer.parseInt(opCode,2)];
+			newinst.setOperationType(operation);
+			switch(operation){
+				case add:
+				case sub:
+				case mul:
+				case div:
+				case and:
+				case or:
+				case xor:
+				case slt:
+				case sll:
+				case srl:
+				case sra:
+						Operand rs1 = new Operand();
+						rs1.setOperandType(OperandType.Register);
+						Operand rs2 = new Operand();
+						rs2.setOperandType(OperandType.Register);
+						Operand rd = new Operand();
+						rd.setOperandType(OperandType.Register);
+						rs1.setValue(Integer.parseInt(inst.substring(5, 10),2));
+						rs2.setValue(Integer.parseInt(inst.substring(10, 15),2));
+						rd.setValue(Integer.parseInt(inst.substring(15, 20),2));
+						newinst.setSourceOperand1(rs1);
+						newinst.setSourceOperand2(rs2);
+						newinst.setDestinationOperand(rd);
+						break;
+				case addi:
+				case andi:
+				case muli:
+				case ori:
+				case slli:
+				case slti:
+				case srai:
+				case srli:
+				case subi:
+				case xori:
+				case divi:
+				case store:
+				case load:
+						Operand rsi1 = new Operand();
+						rsi1.setOperandType(OperandType.Register);
+						Operand rdi = new Operand();
+						rdi.setOperandType(OperandType.Register);
+						rsi1.setValue(Integer.parseInt(inst.substring(5, 10),2));
+						rdi.setValue(Integer.parseInt(inst.substring(10, 15),2));
+						Operand imm = new Operand();
+						imm.setOperandType(OperandType.Immediate);
+						String immd = inst.substring(15,32);
+						if(immd.charAt(0)=='1')
+						{
+							immd = twoscomplement(immd);
+							imm.setValue(-1*Integer.parseInt(immd,2));
+						}
+						else
+						{
+							imm.setValue(Integer.parseInt(immd,2));
+						}
+						newinst.setSourceOperand1(rsi1);
+						newinst.setSourceOperand2(imm);
+						newinst.setDestinationOperand(rdi);
+						break;
+				case jmp:
+						Operand rdj = new Operand();
+						rdj.setOperandType(OperandType.Register);
+						Operand immj = new Operand();
+						immj.setOperandType(OperandType.Immediate);
+						rdj.setValue(Integer.parseInt(inst.substring(5, 10),2));
+						String imme = inst.substring(10,32);
+						if(imme.charAt(0)=='1')
+						{
+							imme = twoscomplement(imme);
+							immj.setValue(-1*Integer.parseInt(imme,2));
+						}
+						else
+						{
+							immj.setValue(Integer.parseInt(imme,2));
+						}
+						newinst.setDestinationOperand(immj);
+						newinst.setSourceOperand1(rdj);
+						break;
+				case end:
+						break;
+				case beq:
+				case bgt:
+				case blt:
+				case bne:
+						Operand rsb = new Operand();
+						rsb.setOperandType(OperandType.Register);
+						Operand rdb = new Operand();
+						rdb.setOperandType(OperandType.Register);
+						Operand immb = new Operand();
+						immb.setOperandType(OperandType.Immediate);
+						rsb.setValue(Integer.parseInt(inst.substring(5, 10),2));
+						rdb.setValue(Integer.parseInt(inst.substring(10, 15),2));
+						String imm1 = inst.substring(15,32);
+						if(imm1.charAt(0)=='1')
+						{
+							imm1 = twoscomplement(imm1);
+							immb.setValue(-1*Integer.parseInt(imm1,2));
+						}
+						else
+						{
+							immb.setValue(Integer.parseInt(imm1,2));
+						}
+						newinst.setSourceOperand1(rsb);
+						newinst.setSourceOperand2(rdb);
+						newinst.setDestinationOperand(immb);
+						break;
+				default:
+						break;						
+			}
+			Instruction previnst = OF_EX_Latch.getInstruction();
+			String opcode_prev = previnst.getOperationType().toString();
+			if( opCode.equals("nop") || opcode.equals("b") || opcode.equal("beq") || opCode.equals("bgt") || opCode.equals("ret")){
+				OF_EX_Latch.setInstruction(newinst);			
+				IF_OF_Latch.setOF_enable(false);
+				OF_EX_Latch.setEX_enable(true);
+			}
+
+			else if( opcode_prev.equals("nop") || opcode_prev.equals("cmp") || opcode_prev.equals("st") || opcode_prev.equals("b") || opcode_prev.equals("beq") || opcode_prev.equals("bgt") || opcode_prev.equals("ret") ){
+				OF_EX_Latch.setInstruction(newinst);			
+				IF_OF_Latch.setOF_enable(false);
+				OF_EX_Latch.setEX_enable(true);
+			}
+
+			else{
+
+				int src1 = containingProcessor.getRegisterFile().getValue(newinst.getSourceOperand1().getValue());
+				int src2 = containingProcessor.getRegisterFile().getValue(newinst.getSourceOperand2().getValue());
+				if( opcode.equals("st")){
+					src2 = containingProcessor.getRegisterFile().getValue(newinst.getDestinationOperand().getValue());
+				}
+				if (opcode.equals("ret")){
+					src1 =                                   //check this out once (ra)
+				}
+				boolean hasSrc1=true;
+				if( opCode.equals("not") || opCode.equals("mov") ){
+					hasSrc1=false;
+				}
+				int dest = containingProcessor.getRegisterFile().getValue(previnst.getDestinationOperand().getValue());
+				if ( opcode_prev.equals("call") ){
+					dest =                                   // ra
+				}
+				boolean hasSrc2=true;
+				if( !opCode.equals("store")){
+					int I = newinst.getSourceOperand2().getValue();
+				}
+			}
+
+
+			
+		}
+	}
+
+}
+
